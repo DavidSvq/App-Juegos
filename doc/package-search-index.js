a40e863e9963d49a78c5c1ee392d65357c4d935a
David <davirt@hotmail.com>
2025-06-24 21:31:52 +0200

    Generación Documentacion con JavaDoc de las Clases Jugador: modelo, repositorio, servicio y controlador

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.juegos.app.repositorio"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.juegos.app.controlador"}];updateSearchResults();
